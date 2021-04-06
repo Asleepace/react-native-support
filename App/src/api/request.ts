ebfddb1486f4c42954f247df0119f5905f28ac37
Colin Teahan <colin_teahan@yahoo.com>
2021-04-06 09:47:25 -0700

    testing chat widget

--- App/src/api/request.ts
+++ App/src/api/request.ts
@@ -4,32 +4,25 @@
  * 
  */
 
-const API = {
-	INCOMING_MESSAGE: 'https://padlet.api.frontapp.com/channels/cha_4ikcv/incoming_messages'
+export const senRequestToFront = () => {
+
 }
 
-const body = {
-	sender: {
-		contact_id: '123',
-		name: 'Colin',
-		handle: 'asdfh2lk3bnmqsn123'
-	},
-	subject: 'Mobile Application - Testing',
-	body: 'This is a test message that was sent from the new widget'
-}
-
-const options = {
-  method: 'POST',
-  headers: {
+fetch('https://padlet.api.frontapp.com/channels/cha_4ikcv/incoming_messages', {
+	method: 'POST',
+	headers: {
 		'Accept': 'application/json',
 		'Content-Type': 'application/json'
 	},
-  body: JSON.stringify(body)
-};
-
-
-export const senRequestToFront = () => {
-	fetch(API.INCOMING_MESSAGE, options)
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-}
+	body: JSON.stringify({
+		sender: {
+			contact_id: '123',
+			handle: 'abcdefg',
+			name: 'colin',
+		},
+		subject: 'Mobile Application - Testing',
+		body: 'This is a test message that was sent from the new widget'
+	})
+})
+.then(response => console.log(response))
+.catch(err => console.error(err))
